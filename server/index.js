9b957b31cbf37c4e3106a6c9bb556ffeb695ba7a
John Whiles <johnwhiles@outlook.com>
2017-02-05 21:21:18 +0000

    get port form process.env

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const Express = require('express');
 
 const server = require('./server.js');
-
+const port = process.env.PORT || 3000
 //runs the server
-server.listen(3000, () => {
-  console.log('listening on port 3000');
+server.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
