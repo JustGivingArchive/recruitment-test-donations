dc5130761eadba6e904d9f69cb708a0f904c8116
John Whiles <johnwhiles@outlook.com>
2017-02-05 10:27:20 +0000

    make basic structure for react components

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const App = () => {
+  return (<h1>hi</h1>);
+};
+
+export default App;
---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../App.js';
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <App />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
