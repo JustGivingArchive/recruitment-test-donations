cae25eddbf705275eeffcb820f467d0a650cb60e
John Whiles <johnwhiles@outlook.com>
2017-02-06 20:23:54 +0000

    remove console logs

--- server/lib/donationrequest.js
+++ server/lib/donationrequest.js
@@ -19,7 +19,6 @@ const makeRequest = (charityId, callback) => {
     response.on('end', () => {
       const parsed = JSON.parse(body);
       const data = parsed.donations;
-      console.log(parsed, data);
       callback(null, data);
     });
   });
